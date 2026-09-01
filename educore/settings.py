a4bed0d7376cc62afa1b4540a862f1b8682516ae
Eddie Dev <olasunkamiabdulrasheed@gmail.com>
2026-09-01 23:06:01 +0100

    installing neccasirly package

--- educore/settings.py
+++ educore/settings.py
@@ -19,9 +19,6 @@ load_dotenv(BASE_DIR / ".env")
 
 
 
-
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
